287727eca4b3fa19a313f00f01c3f2db3af6860d
Anderson Costa <ancosta1995@gmail.com>
2025-11-22 06:20:24 -0300

    corrijindo obfuscator

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
-import obfuscator from 'vite-plugin-obfuscator';
+import obfuscator from 'rollup-plugin-obfuscator';
 
 export default defineConfig({
     plugins: [
@@ -10,6 +10,7 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+
         vue({
             template: {
                 transformAssetUrls: {
@@ -18,27 +19,40 @@ export default defineConfig({
                 },
             },
         }),
+
         tailwindcss(),
-        obfuscator({
-            compact: true,
-            controlFlowFlattening: true,
-            controlFlowFlatteningThreshold: 1,
-            deadCodeInjection: true,
-            deadCodeInjectionThreshold: 1,
-            stringArray: true,
-            stringArrayThreshold: 1,
-            stringArrayEncoding: ['base64'],
-            splitStrings: true,
-            splitStringsChunkLength: 5,
-            disableConsoleOutput: true,
-            transformObjectKeys: true,
-            renameProperties: true,
-            selfDefending: true,
-            debugProtection: true,
-            debugProtectionInterval: true,
-        }),
     ],
+
+    // Aqui entram as config do Rollup
     build: {
         minify: 'esbuild',
+
+        rollupOptions: {
+            plugins: [
+                obfuscator(
+                    {
+                        compact: true,
+                        controlFlowFlattening: true,
+                        controlFlowFlatteningThreshold: 1,
+                        deadCodeInjection: true,
+                        deadCodeInjectionThreshold: 1,
+                        stringArray: true,
+                        stringArrayThreshold: 1,
+                        stringArrayEncoding: ['base64'],
+                        splitStrings: true,
+                        splitStringsChunkLength: 5,
+                        disableConsoleOutput: true,
+                        renameProperties: true,
+                        transformObjectKeys: true,
+                        selfDefending: true,
+                        debugProtection: true,
+                        debugProtectionInterval: true,
+                    },
+                    [
+                        '**/*.js', // arquivos a serem ofuscados
+                    ]
+                ),
+            ],
+        },
     },
 });
